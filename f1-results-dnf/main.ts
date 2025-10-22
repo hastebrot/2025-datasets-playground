3e4467399e37de64c5ad3a98492e54a36c700743
Benjamin Gudehus <hastebrot@gmail.com>
2025-10-22 18:00:42 +0200

    feat: Use arrow with zstd compression.

--- f1-results-dnf/main.ts
+++ f1-results-dnf/main.ts
@@ -1,18 +1,48 @@
-import { fromCSVStream } from "arquero";
+import { ColumnTable, fromArrow, fromArrowStream, fromCSVStream, table, toArrowIPC } from "arquero";
 import { parquetReadObjects } from "hyparquet";
 import { compressors } from "hyparquet-compressors";
 import { parquetWriteBuffer, type ColumnSource, type SchemaElement } from "hyparquet-writer";
 
-async function convertToParquet(inputFilePath: string, outputFilePath: string) {
-  const inputFile = Bun.file(inputFilePath);
-  const outputFile = Bun.file(outputFilePath);
+export async function readCsvTable(inputFile: Bun.BunFile): Promise<ColumnTable> {
+  const inputStream = inputFile.stream().pipeThrough(new TextDecoderStream());
+  return fromCSVStream(inputStream, { header: true });
+}
 
-  const textStream = inputFile.stream().pipeThrough(new TextDecoderStream());
-  const dt = await fromCSVStream(textStream, { header: true });
-  console.log("input size:", inputFile.size, dt.numRows(), dt.numCols());
+export async function readArrowTable(inputFile: Bun.BunFile): Promise<ColumnTable> {
+  const inputBuffer = await inputFile.arrayBuffer();
+  const arrowBuffer = await Bun.zstdDecompress(inputBuffer);
+  return fromArrow(arrowBuffer);
+}
 
+export async function writeArrowTable(outputFile: Bun.BunFile, table: ColumnTable) {
+  const arrowBuffer = toArrowIPC(table, { format: "file" });
+  const outputBuffer = await Bun.zstdCompress(arrowBuffer, { level: 5 });
+  await outputFile.write(outputBuffer);
+}
+
+export async function readParquetData(inputFile: Bun.BunFile) {
+  const inputBuffer = await inputFile.arrayBuffer();
+  const objects = await parquetReadObjects({
+    file: inputBuffer,
+    compressors,
+  });
+  return objects;
+}
+
+export async function writeParquetData(outputFile: Bun.BunFile, table: ColumnTable) {
+  const columnData = await toParquetColumnData(table);
+  const outputBuffer = parquetWriteBuffer({
+    columnData: columnData.columnData,
+    compressed: true,
+    statistics: false,
+  });
+  await outputFile.write(outputBuffer);
+}
+
+export async function toParquetColumnData(dt: ColumnTable) {
   const schema: SchemaElement[] = [];
   const columnData: ColumnSource[] = [];
+
   for (const columnName of dt.columnNames()) {
     schema.push({
       name: columnName,
@@ -25,28 +55,38 @@ async function convertToParquet(inputFilePath: string, outputFilePath: string) {
     });
   }
 
-  const arrayBuffer = parquetWriteBuffer({
-    columnData,
-    compressed: true,
-    statistics: false,
-  });
-  await outputFile.write(arrayBuffer);
-  console.log("output size:", outputFile.size, schema.length);
+  return { schema, columnData };
 }
 
 if (import.meta.main) {
   // https://www.kaggle.com/datasets/rohanrao/formula-1-world-championship-1950-2020/data
   // https://creativecommons.org/publicdomain/zero/1.0/
-  // await convertToParquet(
-  //   "./data/rohanrao--formula-1-world-championship-1950-2020/results.csv",
-  //   "./data/f1-1950-2020-results.parquet",
-  // );
-  // await convertToParquet(
-  //   "./data/rohanrao--formula-1-world-championship-1950-2020/races.csv",
-  //   "./data/f1-1950-2020-races.parquet",
-  // );
 
-  const file = await Bun.file("./data/f1-1950-2020-results.parquet").arrayBuffer();
-  const data = await parquetReadObjects({ file, compressors });
-  console.log(data.length, data.slice(0, 1));
+  // parquet.
+  await writeParquetData(
+    Bun.file("./data/f1-1950-2020-results.parquet"),
+    await readCsvTable(
+      Bun.file("./data/rohanrao--formula-1-world-championship-1950-2020/results.csv"),
+    ),
+  );
+  await writeParquetData(
+    Bun.file("./data/f1-1950-2020-races.parquet"),
+    await readCsvTable(
+      Bun.file("./data/rohanrao--formula-1-world-championship-1950-2020/races.csv"),
+    ),
+  );
+
+  // arrow.
+  await writeArrowTable(
+    Bun.file("./data/f1-1950-2020-results.zstd.arrow"),
+    await readCsvTable(
+      Bun.file("./data/rohanrao--formula-1-world-championship-1950-2020/results.csv"),
+    ),
+  );
+  await writeArrowTable(
+    Bun.file("./data/f1-1950-2020-races.zstd.arrow"),
+    await readCsvTable(
+      Bun.file("./data/rohanrao--formula-1-world-championship-1950-2020/races.csv"),
+    ),
+  );
 }
